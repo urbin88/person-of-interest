c250ec20f7944e8a24fa88a49d2ff3f6417b456c
urbin88 <61000840+urbin88@users.noreply.github.com>
2020-02-13 12:29:22 +0100

    dsg

--- email-address.sh
+++ email-address.sh
@@ -5,4 +5,4 @@ browser="open -a Safari.app "
 read -p "Enter email address: (eg. myBOSS@sucks.com): " EMAIL
 
 $browser https://www.google.com/search?q=intext:\"$EMAIL\"
-$browser http://www.spokeo.com/social/profile?q=$EMAIL
+
